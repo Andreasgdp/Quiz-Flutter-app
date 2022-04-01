90104b7b9ea1adaa9837e2dd828bd2aeb411521f
Andreasgdp <andreasgdp@gmail.com>
2022-04-01 06:44:49 +0200

    Profile screen

--- lib/topics/topics.dart
+++ lib/topics/topics.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:quizapp/services/services.dart';
-import 'package:quizapp/shared/loading.dart';
 import 'package:quizapp/shared/shared.dart';
 import 'package:quizapp/topics/drawer.dart';
 import 'package:quizapp/topics/topics_item.dart';
